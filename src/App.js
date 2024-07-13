baf73ad3274846f082bfcd621a5e596ad0cb93d9
Farshid Kamal <fkamal@Farshids-Laptop.lan>
2024-07-13 17:25:24 +1000

    push 1

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React 02
+          Learn React 03
         </a>
       </header>
     </div>
